b640aaa64e4ef924cdbf15f4bbbfc28e03fed5e6
Alexander <69606161+Bolevander@users.noreply.github.com>
2021-11-22 18:08:57 +0300

    Revert "Revert "Code/max zh/audio controller""
    This reverts commit ff02cca1e8871150279590866acbe306c50777fa.

---
+++ Rescues/Assets/Scripts/Controllers/AuidioController/AudioController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+
+
+namespace Rescues
+{
+    public sealed class AudioController : IAudioController, IAudioSnapsHots
+    {
+        #region Fields
+
+        private readonly AudioMixerData _mixer;
+        private readonly AudioModel _audioModel;
+        private readonly AudioControllerContext _context;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public AudioController()
+        {
+            _mixer = Resources.Load<AudioMixerData>(AssetsPathGameObject.AudioData[AudioDataType.AudioMixer]);
+            _context = Resources.Load<AudioControllerContext>(AssetsPathGameObject.AudioData[AudioDataType.AudioControllerContext]);
+            _audioModel = new AudioModel();
+            _context.BindAuidoController(this);
+            _mixer.Master.GetFloat(_audioModel.MasterVolume, out var currentValue);
+            _audioModel.SetCurrentValue(currentValue);
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void LoadMainMusicTheme(Transform levelParent)
+        {
+            var mainMusicTheme = Resources.Load(AssetsPathGameObject.AudioData[AudioDataType.HotelMusicTheme]);
+            Object.Instantiate(mainMusicTheme, levelParent);
+        }
+
+        #endregion
+
+
+        #region IAudioController
+
+        public void SetMasterVolume(float value)
+        {
+            var checkedValue = _audioModel.CheckValue(value);
+            _mixer.Master.SetFloat(
+                _audioModel.MasterVolume,
+                checkedValue);
+        }
+
+        public void MuteAllSounds() =>
+            _mixer.Master.SetFloat(
+                _audioModel.MasterVolume,
+                _audioModel.Mute());
+
+        public void SetStartVolumeLevel(float value) =>
+            _mixer.Master.SetFloat(
+                _audioModel.MasterVolume,
+                _audioModel.CheckValue(value));
+
+        public void SetEffectsVolume(float value) =>
+            _mixer.Master.SetFloat(
+                _audioModel.EffectsVolume,
+                _audioModel.CheckValue(value));
+
+        public void SetMusicVolume(float value) =>
+             _mixer.Master.SetFloat(
+                 _audioModel.MusicVolume,
+                 _audioModel.CheckValue(value));
+
+        public void SetUiSoundsVolume(float value) =>
+             _mixer.Master.SetFloat(
+                 _audioModel.UiSoundsVolume,
+                 _audioModel.CheckValue(value));
+
+        public void SetVoiceVolume(float value) =>
+            _mixer.Master.SetFloat(
+                 _audioModel.VoiceVolume,
+                 _audioModel.CheckValue(value));
+
+        #endregion
+
+
+        #region IAudioSnapsHots
+
+        public void MuffledMusic() =>
+            _mixer.MuffledMusic.TransitionTo(Time.deltaTime);
+
+        public void UnMuffledMusic() =>
+            _mixer.UnMuffledMusic.TransitionTo(Time.deltaTime); 
+
+        #endregion
+    }
+}
