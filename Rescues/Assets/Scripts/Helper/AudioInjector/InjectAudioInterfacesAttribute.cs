c90a53733ddf04680e48befe0e1cb5e7d4b89d8a
Alexander <69606161+Bolevander@users.noreply.github.com>
2021-11-22 15:41:37 +0300

    Revert "Fixing"

--- Rescues/Assets/Scripts/Helper/AudioInjector/InjectAudioInterfacesAttribute.cs
+++ Rescues/Assets/Scripts/Helper/AudioInjector/InjectAudioInterfacesAttribute.cs
@@ -6,18 +6,8 @@ namespace Rescues
     [AttributeUsage(AttributeTargets.Field)]
     public sealed class InjectAudioInterfacesAttribute : Attribute
     {
-        #region Fields
-
         public readonly string AssetName;
-
-        #endregion
-
-
-        #region ClassLifeCycles
-
         public InjectAudioInterfacesAttribute(string assetName = null) =>
-            AssetName = assetName; 
-
-        #endregion
+            AssetName = assetName;
     }
 }
